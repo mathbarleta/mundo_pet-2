fa88ad15929a98898c7590210b640c89d532b842
MatheusBarleta <104463328+MatheusBarleta@users.noreply.github.com>
2023-09-01 13:17:02 -0300

    Update mundo_pet.sql

--- mundo_pet/Banco/mundo_pet.sql
+++ mundo_pet/Banco/mundo_pet.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 29/06/2023 às 04:26
+-- Tempo de geração: 01/09/2023 às 18:15
 -- Versão do servidor: 10.4.28-MariaDB
 -- Versão do PHP: 8.2.4
 
@@ -24,16 +24,105 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `administrator`
+-- Estrutura para tabela `administrador`
 --
 
-CREATE TABLE `administrator` (
+CREATE TABLE `administrador` (
   `id_admin` int(11) NOT NULL,
   `adm_email` varchar(255) NOT NULL,
   `adm_senha` varchar(255) NOT NULL,
   `adm_nome` char(255) NOT NULL,
   `adm_sobrenome` char(255) NOT NULL,
   `adm_funcao` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `agendamento`
+--
+
+CREATE TABLE `agendamento` (
+  `id_agendamento` int(11) NOT NULL,
+  `id_cliente` int(11) NOT NULL,
+  `id_pet` int(11) NOT NULL,
+  `tipo` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `carrinho`
+--
+
+CREATE TABLE `carrinho` (
+  `id_carrinho` int(11) NOT NULL,
+  `id_cliente` int(11) NOT NULL,
+  `id_produto` int(11) NOT NULL,
+  `estado` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `id_cliente` int(11) NOT NULL,
+  `nome` char(255) NOT NULL,
+  `sobrenome` char(255) NOT NULL,
+  `cpf` char(14) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `telefone1` int(20) NOT NULL,
+  `telefone2` int(20) DEFAULT NULL,
+  `cep` int(10) NOT NULL,
+  `cidade` varchar(255) NOT NULL,
+  `rua` varchar(255) NOT NULL,
+  `numero` int(10) NOT NULL,
+  `dataNas` date NOT NULL,
+  `senha` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `cliente`
+--
+
+INSERT INTO `cliente` (`id_cliente`, `nome`, `sobrenome`, `cpf`, `email`, `telefone1`, `telefone2`, `cep`, `cidade`, `rua`, `numero`, `dataNas`, `senha`) VALUES
+(23, 'Matheus Henrique', 'Barleta Dias', '108.763.899-22', 'matheusbarleta@hotmail.com', 0, NULL, 0, '', '', 0, '2005-09-21', '621300');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `fornecedor`
+--
+
+CREATE TABLE `fornecedor` (
+  `id_produto` int(11) NOT NULL,
+  `id_pedido` int(11) NOT NULL,
+  `nome_fantasia` varchar(255) NOT NULL,
+  `CNPJ` int(11) NOT NULL,
+  `razao_social` varchar(255) NOT NULL,
+  `endereco` varchar(255) NOT NULL,
+  `bairro` varchar(255) NOT NULL,
+  `cidade` varchar(255) NOT NULL,
+  `complemento` varchar(255) NOT NULL,
+  `CEP` int(11) NOT NULL,
+  `telefone` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `itens_carrinho`
+--
+
+CREATE TABLE `itens_carrinho` (
+  `id_item` int(11) NOT NULL,
+  `id_carrinho` int(11) NOT NULL,
+  `id_produto` int(11) NOT NULL,
+  `quantidade` int(11) NOT NULL,
+  `preco` decimal(10,0) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -56,50 +145,54 @@ CREATE TABLE `login` (
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `orders`
+-- Estrutura para tabela `pedido`
 --
 
-CREATE TABLE `orders` (
-  `id_order` int(11) NOT NULL,
-  `pro_barcode` varchar(255) NOT NULL,
-  `ord_quantidade` int(11) NOT NULL,
-  `ord_pagamento` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `products`
---
-
-CREATE TABLE `products` (
-  `adm_email` varchar(255) NOT NULL,
-  `pro_barcode` varchar(255) NOT NULL,
-  `pro_categoria` varchar(255) NOT NULL,
-  `pro_preco` decimal(10,2) NOT NULL,
-  `pro_tamanho` varchar(50) NOT NULL,
-  `pro_imagem` varchar(255) NOT NULL,
-  `id_administrator` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `tb_client`
---
-
-CREATE TABLE `tb_client` (
+CREATE TABLE `pedido` (
+  `id_pedido` int(11) NOT NULL,
   `id_cliente` int(11) NOT NULL,
-  `cli_nome` char(255) NOT NULL,
-  `cli_sobrenome` char(255) NOT NULL,
-  `cli_cpf` int(11) NOT NULL,
-  `cli_email` varchar(255) NOT NULL,
-  `cli_telefone1` int(20) NOT NULL,
-  `cli_telefone2` int(20) DEFAULT NULL,
-  `cli_cep` int(10) NOT NULL,
-  `cli_cidade` varchar(255) NOT NULL,
-  `cli_rua` varchar(255) NOT NULL,
-  `cli_numero` int(10) NOT NULL
+  `id_produto` int(11) NOT NULL,
+  `id_fornecedor` int(11) NOT NULL,
+  `id_pagamento` int(11) NOT NULL,
+  `id_entrega` int(11) NOT NULL,
+  `descricao` text NOT NULL,
+  `valor` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `pet`
+--
+
+CREATE TABLE `pet` (
+  `id_pet` int(11) NOT NULL,
+  `id_cliente` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `raca` varchar(255) NOT NULL,
+  `peso` float NOT NULL,
+  `sexo` varchar(255) NOT NULL,
+  `porte` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `produto`
+--
+
+CREATE TABLE `produto` (
+  `id_produto` int(11) NOT NULL,
+  `id_fornecedor` int(11) NOT NULL,
+  `id_pedido` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `cod_barras` varchar(255) NOT NULL,
+  `descricao` text NOT NULL,
+  `preco` float NOT NULL,
+  `categoria` varchar(255) NOT NULL,
+  `quantidade` int(11) NOT NULL,
+  `ingridientes` text NOT NULL,
+  `data_validade` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -107,10 +200,42 @@ CREATE TABLE `tb_client` (
 --
 
 --
--- Índices de tabela `administrator`
+-- Índices de tabela `administrador`
 --
-ALTER TABLE `administrator`
+ALTER TABLE `administrador`
   ADD PRIMARY KEY (`id_admin`);
+
+--
+-- Índices de tabela `agendamento`
+--
+ALTER TABLE `agendamento`
+  ADD PRIMARY KEY (`id_agendamento`),
+  ADD KEY `agendamento/cliente` (`id_cliente`),
+  ADD KEY `agendamento/pet` (`id_pet`);
+
+--
+-- Índices de tabela `carrinho`
+--
+ALTER TABLE `carrinho`
+  ADD PRIMARY KEY (`id_carrinho`);
+
+--
+-- Índices de tabela `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`id_cliente`);
+
+--
+-- Índices de tabela `fornecedor`
+--
+ALTER TABLE `fornecedor`
+  ADD KEY `fornecedor/produto` (`id_produto`);
+
+--
+-- Índices de tabela `itens_carrinho`
+--
+ALTER TABLE `itens_carrinho`
+  ADD PRIMARY KEY (`id_item`);
 
 --
 -- Índices de tabela `login`
@@ -120,33 +245,59 @@ ALTER TABLE `login`
   ADD KEY `id_cliente` (`id_cliente`);
 
 --
--- Índices de tabela `orders`
+-- Índices de tabela `pedido`
 --
-ALTER TABLE `orders`
-  ADD PRIMARY KEY (`id_order`);
+ALTER TABLE `pedido`
+  ADD PRIMARY KEY (`id_pedido`),
+  ADD KEY `pedido/produto` (`id_produto`),
+  ADD KEY `pedido/cliente` (`id_cliente`);
 
 --
--- Índices de tabela `products`
+-- Índices de tabela `pet`
 --
-ALTER TABLE `products`
-  ADD PRIMARY KEY (`pro_barcode`),
-  ADD KEY `id_administrator` (`id_administrator`);
+ALTER TABLE `pet`
+  ADD PRIMARY KEY (`id_pet`),
+  ADD KEY `pet/cliente` (`id_cliente`);
 
 --
--- Índices de tabela `tb_client`
+-- Índices de tabela `produto`
 --
-ALTER TABLE `tb_client`
-  ADD PRIMARY KEY (`id_cliente`);
+ALTER TABLE `produto`
+  ADD PRIMARY KEY (`id_produto`);
 
 --
 -- AUTO_INCREMENT para tabelas despejadas
 --
 
 --
--- AUTO_INCREMENT de tabela `administrator`
+-- AUTO_INCREMENT de tabela `administrador`
 --
-ALTER TABLE `administrator`
+ALTER TABLE `administrador`
   MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `agendamento`
+--
+ALTER TABLE `agendamento`
+  MODIFY `id_agendamento` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `carrinho`
+--
+ALTER TABLE `carrinho`
+  MODIFY `id_carrinho` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+
+--
+-- AUTO_INCREMENT de tabela `itens_carrinho`
+--
+ALTER TABLE `itens_carrinho`
+  MODIFY `id_item` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de tabela `login`
@@ -155,32 +306,58 @@ ALTER TABLE `login`
   MODIFY `id_login` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de tabela `orders`
+-- AUTO_INCREMENT de tabela `pedido`
 --
-ALTER TABLE `orders`
-  MODIFY `id_order` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `pedido`
+  MODIFY `id_pedido` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de tabela `tb_client`
+-- AUTO_INCREMENT de tabela `pet`
 --
-ALTER TABLE `tb_client`
-  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `pet`
+  MODIFY `id_pet` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `produto`
+--
+ALTER TABLE `produto`
+  MODIFY `id_produto` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Restrições para tabelas despejadas
 --
 
 --
+-- Restrições para tabelas `agendamento`
+--
+ALTER TABLE `agendamento`
+  ADD CONSTRAINT `agendamento/cliente` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`id_cliente`),
+  ADD CONSTRAINT `agendamento/pet` FOREIGN KEY (`id_pet`) REFERENCES `pet` (`id_pet`);
+
+--
+-- Restrições para tabelas `fornecedor`
+--
+ALTER TABLE `fornecedor`
+  ADD CONSTRAINT `fornecedor/produto` FOREIGN KEY (`id_produto`) REFERENCES `produto` (`id_produto`);
+
+--
 -- Restrições para tabelas `login`
 --
 ALTER TABLE `login`
-  ADD CONSTRAINT `login_ibfk_1` FOREIGN KEY (`id_cliente`) REFERENCES `tb_client` (`id_cliente`);
+  ADD CONSTRAINT `login_ibfk_1` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`id_cliente`);
 
 --
--- Restrições para tabelas `products`
+-- Restrições para tabelas `pedido`
 --
-ALTER TABLE `products`
-  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`id_administrator`) REFERENCES `administrator` (`id_admin`);
+ALTER TABLE `pedido`
+  ADD CONSTRAINT `pedido/cliente` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`id_cliente`),
+  ADD CONSTRAINT `pedido/produto` FOREIGN KEY (`id_produto`) REFERENCES `produto` (`id_produto`);
+
+--
+-- Restrições para tabelas `pet`
+--
+ALTER TABLE `pet`
+  ADD CONSTRAINT `pet/cliente` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`id_cliente`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
